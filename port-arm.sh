a6521c14eb620656065eaf7402e746cb7f060552
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-08-08 16:54:07 +0530

    scripts: update
    1. Shortened output filename.
    
    2. Reworked sed to replace just assert(); and not match all quotes with device name in it.

--- port-arm.sh
+++ port-arm.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 updater=META-INF/com/google/android/updater-script
 mkdir -p boot
+
 [ `unzip -Z1 $2 | grep compatibility.zip` ] && zip -d $2 compatibility.zip
+
 unzip $2 $updater boot.img -d .
-sed -i 's/\"mido\"/getprop\(\"ro.product.device\"\)/g' $updater
-sed -i 's/\"oxygen\"/getprop\(\"ro.product.device\"\)/g' $updater
+sed -i 's/assert(.*);/# Original Zip Script/g' $updater
 sed -i 's/cust/vendor/g' $updater
 sed -i 's/boot\.img/boot\_G\.img/g' $updater
 
@@ -14,8 +16,8 @@ mv tmp/mochi ./mochi
 cat tmp/$updater >> $updater
 echo -e "\nui_print(\"ROM ported to 10or G with MOCHI's autoporting script [TG: @mochi_wwww]\");" >> $updater
 zip -ur $2 mochi/*
-
 zip --update $2 $updater
+
 ./bin/unpackbootimg-arm -i boot.img -o boot && boot=boot/boot.img
 cp bin/kud-2.0 $boot-zImage
 sed -i 's/enforcing/permissive/g' $boot-cmdline
@@ -23,6 +25,7 @@ sed -i 's/$/ androidboot\.selinux\=\permissive/' $boot-cmdline
 sed -i 's/$/ firmware_class\.path\=\/vendor\/firmware\_mnt\/image/' $boot-cmdline
 cat $boot-cmdline | sed 's/ /\n/g' | sort -u | tr '\n' ' ' > boot/bcl && rm $boot-cmdline && mv boot/bcl $boot-cmdline
 ./bin/mkbootimg-arm --kernel $boot-zImage --ramdisk $boot-ramdisk.gz --cmdline "$(cat $boot-cmdline)" --base $(cat $boot-base) --pagesize $(cat $boot-pagesize) -o boot_G.img
+
 zip -ur $2 boot_G.img && rm boot.img && rm boot_G.img
 zip -d $2 boot.img
-mv $2 "MOCHI_PORTS_10or_G_${2::-4}_` [ $1 == "m" ] && echo "mido" || echo "oxygen" `_PORT.zip" && rm -rf META-INF && rm -rf boot && rm -rf tmp && rm -rf mochi
+mv $2 "MOCHI_PORTS_` [ $1 == "m" ] && echo "M" || echo "O" `2G_${2::-4}.zip" && rm -rf META-INF && rm -rf boot && rm -rf tmp && rm -rf mochi
