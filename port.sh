bb623f04e077fe5dc97de8784a6754a30c037358
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-08-29 00:24:53 +0530

    scripts: update for vince and 4.9 kernel

--- port.sh
+++ port.sh
@@ -12,14 +12,21 @@ sed -i 's/boot\.img/boot\_G\.img/g' $updater
 
 unzip bin/core.zip -d tmp
 mv tmp/mochi ./mochi
-[ $1 == "m" ] && rm -rf mochi/oxygen | rm tmp/$updater | mv tmp/${updater}-mido tmp/$updater || rm -rf mochi/mido
+
+[ $1 == "m" ] && rm -rf mochi/oxygen && rm -rf mochi/vince | rm tmp/$updater | mv tmp/${updater}-mido tmp/$updater || rm -rf mochi/mido
+[ $1 == "o" ] && rm -rf mochi/mido && rm -rf mochi/vince | rm tmp/$updater | mv tmp/${updater}-oxygen tmp/$updater || rm -rf mochi/oxygen
+[ $1 == "v" ] && rm -rf mochi/mido && rm -rf mochi/oxygen | rm tmp/$updater | mv tmp/${updater}-vince tmp/$updater || rm -rf mochi/vince
+
 cat tmp/$updater >> $updater
 echo -e "\nui_print(\"ROM ported to 10or G with MOCHI's autoporting script [TG: @mochi_wwww]\");" >> $updater
 zip -ur $2 mochi/*
 zip --update $2 $updater
 
 ./bin/unpackbootimg -i boot.img -o boot && boot=boot/boot.img
-cp bin/kud-2.0 $boot-zImage
+
+[ $3 == '3' ] && cp bin/kud-3.18-2.1.2 $boot-zImage && kver=3.18.X
+[ $3 == '4' ] && cp bin/kud-4.9-1.2 $boot-zImage && kver=4.9.X
+
 sed -i 's/enforcing/permissive/g' $boot-cmdline
 sed -i 's/$/ androidboot\.selinux\=\permissive/' $boot-cmdline
 sed -i 's/$/ firmware_class\.path\=\/vendor\/firmware\_mnt\/image/' $boot-cmdline
@@ -35,4 +42,5 @@ cat $boot-cmdline | sed 's/ /\n/g' | sort -u | tr '\n' ' ' > boot/bcl && rm $boo
 
 zip -ur $2 boot_G.img && rm boot.img && rm boot_G.img
 zip -d $2 boot.img
-mv $2 "MOCHI_PORTS_` [ $1 == "m" ] && echo "M" || echo "O" `2G_${2::-4}.zip" && rm -rf META-INF && rm -rf boot && rm -rf tmp && rm -rf mochi
+
+mv $2 "MOCHI_PORTS_` echo ${1^^} `2G_${2::-4}_` echo $kver `.zip" && rm -rf META-INF && rm -rf boot && rm -rf tmp && rm -rf mochi
